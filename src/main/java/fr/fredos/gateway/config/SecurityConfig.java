1c57be1fa59186100064c25cf5a9d96ed10e3052
fredo <fred@autogrif.com>
2021-12-27 18:14:07 +0100

    fix

--- src/main/java/fr/fredos/gateway/config/SecurityConfig.java
+++ src/main/java/fr/fredos/gateway/config/SecurityConfig.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
 public class SecurityConfig {
 	@Bean
 	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 		http.authorizeExchange().anyExchange()
-				.authenticated().and().oauth2Login(); // to redirect to oauth2 login page.
+				.authenticated().and().oauth2Login().and().securityContextRepository(NoOpServerSecurityContextRepository.getInstance()); // to redirect to oauth2 login page.
 		//http.csrf().disable();
 		return http.build();
 	}
