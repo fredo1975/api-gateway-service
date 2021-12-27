8cf329521424d0243cca2f5d6019bf3d89325804
fredo <fred@autogrif.com>
2021-12-27 12:07:29 +0100

    fix

--- src/main/java/fr/fredos/gateway/controller/ApiGatewayController.java
+++ src/main/java/fr/fredos/gateway/controller/ApiGatewayController.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2Aut
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.WebSession;
 
 import reactor.core.publisher.Mono;
 
@@ -25,6 +26,12 @@ public class ApiGatewayController {
 	public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
 		return Mono.just(authorizedClient.getAccessToken().getTokenValue());
 	}
+	
+	@GetMapping("/session")
+	public Mono<String> index(WebSession session) {
+		return Mono.just(session.getId());
+	}
+
 	
 	@GetMapping("/whoami")
 	@ResponseBody
